a4f7d67f77584d9aca15dfa7c56bc3bed79f7733
JonathanAstorga <159974448+JonathanAstorga@users.noreply.github.com>
2024-02-14 16:36:59 -0700

    Add files via upload

---
+++ BancoDominio/src/main/java/entidadesdominio/Cliente.java
@@ -0,0 +1,143 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidadesdominio;
+
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Cliente {
+    private int cliente_id;
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private Date fechaNac; 
+    private int usuario_id;
+    private int direccion_id;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nombre, String apellidoPaterno, String apellidoMaterno, Date fechaNac, int usuario_id, int direccion_id) {
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.fechaNac = fechaNac;
+        this.usuario_id = usuario_id;
+        this.direccion_id = direccion_id;
+    }
+
+    public int getCliente_id() {
+        return cliente_id;
+    }
+
+    public void setCliente_id(int cliente_id) {
+        this.cliente_id = cliente_id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public Date getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(Date fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public int getUsuario_id() {
+        return usuario_id;
+    }
+
+    public void setUsuario_id(int usuario_id) {
+        this.usuario_id = usuario_id;
+    }
+
+    public int getDireccion_id() {
+        return direccion_id;
+    }
+
+    public void setDireccion_id(int direccion_id) {
+        this.direccion_id = direccion_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "cliente_id=" + cliente_id + ", nombre=" + nombre + ", apellidoPaterno=" + apellidoPaterno + ", apellidoMaterno=" + apellidoMaterno + ", fechaNac=" + fechaNac + ", usuario_id=" + usuario_id + ", direccion_id=" + direccion_id + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + this.cliente_id;
+        hash = 67 * hash + Objects.hashCode(this.nombre);
+        hash = 67 * hash + Objects.hashCode(this.apellidoPaterno);
+        hash = 67 * hash + Objects.hashCode(this.apellidoMaterno);
+        hash = 67 * hash + Objects.hashCode(this.fechaNac);
+        hash = 67 * hash + this.usuario_id;
+        hash = 67 * hash + this.direccion_id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        if (this.cliente_id != other.cliente_id) {
+            return false;
+        }
+        if (this.usuario_id != other.usuario_id) {
+            return false;
+        }
+        if (this.direccion_id != other.direccion_id) {
+            return false;
+        }
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        if (!Objects.equals(this.apellidoPaterno, other.apellidoPaterno)) {
+            return false;
+        }
+        if (!Objects.equals(this.apellidoMaterno, other.apellidoMaterno)) {
+            return false;
+        }
+        return Objects.equals(this.fechaNac, other.fechaNac);
+    }
+    
+    
+}
