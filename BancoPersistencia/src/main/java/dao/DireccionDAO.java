053829586c08a096e285b42bcd9e52b852c32cc3
WilberQuintero <115685080+WilberQuintero@users.noreply.github.com>
2024-02-16 02:55:48 -0600

    BancoPersistencia DAOs hechos...
    Ya estan todos los DAOs...

--- BancoPersistencia/src/main/java/dao/DireccionDAO.java
+++ BancoPersistencia/src/main/java/dao/DireccionDAO.java
@@ -8,6 +8,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.logging.Level;
 import entidadesdominio.Direccion;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
@@ -31,25 +32,79 @@ public class DireccionDAO implements IDireccionDAO{
     
 
     @Override
-    public Direccion agregar(Direccion cliente) throws PersistenciaException {
+    public Direccion agregarDireccion(Direccion direccion) throws PersistenciaException {
+        
+        String codigoSQL = "INSERT INTO DIRECCIONES (calle, colonia, numero) VALUES (?,?,?)";
 
+        try (    Connection conexion = this.conexionBD.crearConexion(); // establecemos la conexion con la bd
+                 PreparedStatement comandoSQL = conexion.prepareStatement(codigoSQL, Statement.RETURN_GENERATED_KEYS);) {
+
+            // 3. mandar los valores de la dirección al comandoSQL
+            comandoSQL.setString(1, direccion.getCalle());
+            comandoSQL.setString(2, direccion.getColonia());
+            comandoSQL.setString(3, direccion.getNumero());
+
+            // 4. Ejecutamos el comando o lo enviamos a la BD
+            int registros = comandoSQL.executeUpdate();
+            LOG.log(Level.INFO, "Se agregaron con éxito {0} registros", registros);
+
+            // obtener el conjunto de resultados que tiene o contiene las llaves generadas durante el registro o inserción
+            ResultSet registro = comandoSQL.getGeneratedKeys();
+
+            // nos posicionamos en el primer registro o en el siguiente disponible.
+            registro.next();
+
+            Direccion direccionNueva = new Direccion(
+               registro.getInt(1),
+                    direccion.getCalle(),
+                   direccion.getColonia(),
+                   direccion.getNumero()
+            );
+            // regresamos la dirección
+            return direccionNueva;
+
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE, "No se agregó con éxito la dirección", e);
+            throw new PersistenciaException("No se pudo guardar la dirección ", e);
+        }
+    }
+    
+
+    
+    @Override
+    public Direccion actualizarDireccion(Direccion direccion) throws PersistenciaException {
+
+        String codigoSQL = "UPDATE DIRECCIONES SET CALLE=?, COLONIA=?, NUMERO=? WHERE id=?";
+        
+        try (Connection conexion = this.conexionBD.crearConexion();
+            PreparedStatement comandoSQL = conexion.prepareStatement(codigoSQL)) {
+            
+           comandoSQL.setString(1, direccion.getCalle());
+           comandoSQL.setString(2, direccion.getColonia());
+           comandoSQL.setString(3,direccion.getNumero());
+           
+           int registros = comandoSQL.executeUpdate();
+           LOG.log(Level.INFO, "Se actualizó con exito {0} ", registros);
+       return consultarDireccion(direccion.getDireccion_id());
+           
+        } catch (Exception e){
+           LOG.log(Level.SEVERE, "No se pudo actualizar la direccion", e);
+           throw new PersistenciaException("No se pudo actualizar la direccion ", e);
+        }
+        
+        
     }
 
     
     @Override
-    public Direccion actualizar(Direccion cliente) throws PersistenciaException {
-
-    }
-
-    
-     public Direccion consultarDireccion(int direccionId) throws PersistenciaException {
+     public Direccion consultarDireccion(int id) throws PersistenciaException {
   
-            String consulta = "SELECT * FROM DIRECCIONES WHERE id = (?)";
+            String codigoSQL = "SELECT * FROM DIRECCIONES WHERE id=?";
 
              try (Connection conexion = this.conexionBD.crearConexion();
-                PreparedStatement comandoSQL = conexion.prepareStatement(consulta)) {
+                PreparedStatement comandoSQL = conexion.prepareStatement(codigoSQL)) {
 
-            comandoSQL.setInt(1, direccionId);
+            comandoSQL.setInt(1, id);
             ResultSet resultado = comandoSQL.executeQuery();
 
             resultado.next();
@@ -72,12 +127,12 @@ public class DireccionDAO implements IDireccionDAO{
      
     @Override
     public List<Direccion> consultarTodasDirecciones() throws PersistenciaException {
-        String codigoSQL = "SELECT * FROM DIRECCIONES";
+String codigoSQL = "SELECT * FROM DIRECCIONES";
         List<Direccion> direccionesLista = new ArrayList<>();
 
        try (Connection conexion = conexionBD.crearConexion();
          PreparedStatement comandoSQL = conexion.prepareStatement(codigoSQL);
-         
+
                ResultSet resultado = comandoSQL.executeQuery()) {
 
            while (resultado.next()) {
@@ -85,7 +140,7 @@ public class DireccionDAO implements IDireccionDAO{
                 String calle = resultado.getString("calle");
                 String colonia = resultado.getString("colonia");
                 String numero = resultado.getString("numero");
-                
+
 
                 Direccion direccion = new Direccion(direccion_id, calle, colonia, numero);
                 direccionesLista.add(direccion);
@@ -99,8 +154,7 @@ public class DireccionDAO implements IDireccionDAO{
             throw new PersistenciaException("No se pudieron consultar los clientes", e);
         }
     }
-
-    
+     
 }
     
 
---
+++ BancoPersistencia/src/main/java/dao/TransferenciaDAO.java
@@ -0,0 +1,136 @@
+
+package dao;
+
+import ConexionBD.IConexionBD;
+import Excepciones.PersistenciaException;
+import entidadesdominio.Transferencia;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Wilber
+ */
+public class TransferenciaDAO implements ITransferenciaDAO{
+
+    final IConexionBD conexionBD;
+    private static final Logger LOG = Logger.getLogger(DireccionDAO.class.getName());
+
+    
+    public TransferenciaDAO(IConexionBD conexionBD) {
+        this.conexionBD = conexionBD;
+    }
+
+    
+    @Override
+    public Transferencia agregarDireccion(Transferencia transferencia) throws PersistenciaException {
+
+        String codigoSQL = "INSERT INTO TRANSFERENCIAS (cuentaDestino, monto, fecha, cuenta_id) VALUES (?,?,?)";
+        
+          try (    Connection conexion = this.conexionBD.crearConexion(); // establecemos la conexion con la bd
+                 PreparedStatement comandoSQL = conexion.prepareStatement(codigoSQL, Statement.RETURN_GENERATED_KEYS);) {
+              
+            comandoSQL.setInt(1,transferencia.getCuentaDestino());
+            comandoSQL.setInt(2, transferencia.getMonto());
+            comandoSQL.setDate(4, (Date) transferencia.getFecha());
+            comandoSQL.setInt(4,transferencia.getCuenta_id());
+        
+              int registros = comandoSQL.executeUpdate();
+            LOG.log(Level.INFO, "Se agregaron con éxito {0} registros", registros);
+            
+                        ResultSet registro = comandoSQL.getGeneratedKeys();
+                        registro.next();
+                        
+                        Transferencia transferenciaNueva = new Transferencia(
+               registro.getInt(1),
+                transferencia.getCuentaDestino(),
+                      transferencia.getMonto(),
+                      transferencia.getFecha(),
+                   transferencia.getCuenta_id()
+            );
+            // regresamos la dirección
+            return transferenciaNueva;
+          
+          } catch (Exception e) {
+            LOG.log(Level.SEVERE, "No se agregó con éxito la dirección", e);
+            throw new PersistenciaException("No se pudo guardar la dirección ", e);
+        }
+    }
+    
+
+
+    @Override
+    public Transferencia consultarTransferencia(int id) throws PersistenciaException {
+   
+        
+            String codigoSQL = "SELECT * FROM DIRECCIONES WHERE id=?";
+
+             try (Connection conexion = this.conexionBD.crearConexion();
+                PreparedStatement comandoSQL = conexion.prepareStatement(codigoSQL)) {
+
+            comandoSQL.setInt(1, id);
+            ResultSet registro = comandoSQL.executeQuery();
+
+            registro.next();
+
+            Transferencia transferenciaConsultada = new Transferencia(
+                    registro.getInt(1),
+                    registro.getInt(2),
+                    registro.getDate(3),
+                    registro.getInt(4)
+
+            );
+
+            return transferenciaConsultada;
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE, "Dirección no encontrada", e);
+            throw new PersistenciaException("No se ha encontrado ninguna dirección", e);
+        }    
+    
+    }
+
+    @Override
+    public List<Transferencia> consultarTodasTransferencia() throws PersistenciaException {
+            
+            String codigoSQL = "SELECT * FROM DIRECCIONES";
+             
+        List<Transferencia> transferenciasLista = new ArrayList<>();
+
+       try (Connection conexion = conexionBD.crearConexion();
+         PreparedStatement comandoSQL = conexion.prepareStatement(codigoSQL);
+
+               ResultSet registro = comandoSQL.executeQuery()) {
+
+           while (registro.next()) {
+                int transaccion_id = registro.getInt("transferencia_id");
+                int cuentaDestino = registro.getInt("cuentaDestino");
+                int monto = registro.getInt("monto");
+                Date fecha = registro.getDate("fecha");
+                int cuenta_id = registro.getInt("cuenta_id");
+
+
+                Transferencia transferencia = new Transferencia(transaccion_id, cuentaDestino, monto, fecha, cuenta_id);
+                transferenciasLista.add(transferencia);
+            }
+
+            LOG.log(Level.INFO, "Se consultaron {0} clientes", transferenciasLista.size());
+            return transferenciasLista;
+
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE, "No se pudieron obtener los clientes", e);
+            throw new PersistenciaException("No se pudieron consultar los clientes", e);
+        }
+     }
+
+    }
+    
+    
+    
+
