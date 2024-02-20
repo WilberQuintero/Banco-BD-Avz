3441de5697449169e4faad2f54e5657a8e87c13e
JonathanAstorga <159974448+JonathanAstorga@users.noreply.github.com>
2024-02-19 17:53:34 -0700

    Add files via upload

--- TRIGGERS BDBANCO.sql
+++ TRIGGERS BDBANCO.sql
@@ -1,6 +1,7 @@
 -- triggers de banco
 use avance1bd;
 
+-- cuenta
 delimiter //
 create trigger numeroCuenta
 before insert on cuentas
@@ -11,7 +12,27 @@ begin
     set new.numeroCuenta = lpad(numCue, 16, '0');
     set new.fechaApertura = now();
 end//
+
 delimiter ;
 
- -- drop trigger numeroCuenta;
+-- Retiro Sin Cuenta
+delimiter //
+
+create trigger folio
+before insert on retirossincuenta
+for each row
+begin
+    declare max_folio bigint;
+    declare contra bigint;
+    select max(folio) into max_folio from retirossincuenta;
+    set new.folio = coalesce(max_folio, 0) + 1;
+    set new.fecha = now();
+    set new.fechaFin = date_add(new.fecha, INTERVAL 10 MINUTE);
+    set contra = floor(rand() * 99999999);
+    set new.contra = lpad(contra, 8, '0');
+end//
+
+delimiter ;
+
+ --  drop trigger folio;
 
